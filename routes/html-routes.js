db83c7f72fb3513571c169c6175b023be3211416
Pedro Alburqueque <prolsu88@gmail.com>
2021-02-19 09:30:56 -0500

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,15 @@
+const path = require("path");
+
 module.exports = function (app) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "./index.html"));
+    });
+    
     app.get("/exercise", (req, res) => {
-        location.replace("/exercise")
-    })
-}
+        res.sendFile(path.join(__dirname, "./exercise.html"));
+    });
+    
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "./stats.html")):
+    });
+};
